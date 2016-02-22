cbdb74eec7ebfb3085006b1c5e24fabac0105aa0
Elijah Bullard <elijahbullard@Elijahs-MacBook-Pro.local>
2016-02-21 19:02:54 -0500

    completed 2nd quickstart video

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -53,18 +53,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             twitterClient.GET("1.1/account/verify_credentials.json", parameters: nil, progress: nil, success: {
                 (task: NSURLSessionDataTask, response: AnyObject?) -> Void in
 
-                let user = response as! NSDictionary
-                print("name: \(user["name"])")
+                let userDictionary = response as! NSDictionary
+                
+//                print("user: \(user)")
+                
+                let user = User(dictionary: userDictionary)
+                
+                print("name: \(user.name)")
+                print("screenname: \(user.screenname)")
+                print("profile url: \(user.profileUrl)")
+                print("description: \(user.tagline)")
                 
                 }, failure: { (task: NSURLSessionDataTask?, error: NSError) -> Void in
                     
             })
             
             twitterClient.GET("1.1/statuses/home_timeline.json", parameters: nil, progress: nil, success: { (task: NSURLSessionDataTask, response: AnyObject?) -> Void in
-                let tweets = response as! [NSDictionary]
+                let dictionaries = response as! [NSDictionary]
+                
+                let tweets = Tweet.tweetsWithArray(dictionaries)
+                
                 
                 for tweet in tweets {
-                    print("\(tweet["text"]!)")
+                    print("\(tweet.text!)")
                 }
                 
                 }, failure: { (task: NSURLSessionDataTask?, error: NSError) -> Void in
---
+++ Twitter/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Elijah Bullard on 2/21/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+    var name: NSString?
+    var screenname: NSString?
+    var profileUrl: NSURL?
+    var tagline: NSString?
+    
+    init(dictionary: NSDictionary) {
+        
+        name = dictionary["name"] as? String
+        screenname = dictionary["screen_name"] as? String
+        
+        let profileUrlString = dictionary["profile_image_url_https"] as? String
+        if let profileUrlString = profileUrlString {
+            profileUrl = NSURL(string: profileUrlString)
+        }
+        
+        let tagline = dictionary["description"] as? String
+        
+    }
+    
+}
